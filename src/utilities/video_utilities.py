5f0f915aa9aa283cb0bb13c26c6249de359f622f
youpi0214 <ouambay@gmail.com>
2023-01-03 22:56:37 -0500

    -converstion de path relatif a absolue ajouter - dialog pour changer les option de telechargement

---
+++ src/utilities/video_utilities.py
@@ -0,0 +1,13 @@
+import enum
+
+
+class SupportedVideoResolution(enum.Enum):
+    RES_144p = '144p'
+    RES_240p = '240p'
+    RES_360p = '360p'
+    RES_480p = '480p'
+    RES_720p = '720p'
+    RES_1080p = '1080p'
+    RES_1440p = '1440p'
+    RES_2160p = '2160p'
+    RES_4320p = '4320p'
