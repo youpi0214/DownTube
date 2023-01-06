7e3a1fed3522f646f23fded8ed16c7b6fe09ae37
youpi0214 <ouambay@gmail.com>
2023-01-05 20:45:47 -0500

    - dialog pour changer les option de telechargement est terminé

--- src/gui/download_view.py
+++ src/gui/download_view.py
@@ -1,13 +1,19 @@
+from PyQt5.QtCore import pyqtSlot
 from PyQt5.QtWidgets import *
 from PyQt5 import uic
 from PyQt5.uic.properties import QtCore
 from pytube import YouTube
 from pytube.exceptions import PytubeError
 from utilities.general_utilities import relative_to_abs_path, does_path_exist
+from utilities.video_utilities import SupportedAudioSampleRate, SupportedVideoResolution, SupportedVideoFPS
 
 DOWNLOAD_PATH = 'download_path'
 EDIT_LINE_VALUE = 'text_edit_value'
 DEFAULT_PATH_CHECKED = 'default_path'
+VIDEO_RES = 'video_res'
+VIDEO_FPS = 'video_fps'
+AUDIO_SAMPLERATE = 'audio_samplerate'
+PROGRESSIVE = 'progressive'
 
 
 class DownloadOptionPopup(QDialog):
@@ -18,36 +24,66 @@ class DownloadOptionPopup(QDialog):
 
         self.line_edit_path: QLineEdit
         self.default_path_checkBox: QCheckBox
+        self.video_res: QComboBox
+        self.video_fps: QComboBox
+        self.audio_sample_rate: QComboBox
+        self.progressive_checkbox: QCheckBox
         self.__init_state()
 
-    @property
-    def edit_path(self) -> QLineEdit:
-        return self.line_edit_path
+    def on_default_checkBox(self):
+        if self.default_path_checkBox.isChecked():
+            self.line_edit_path.setDisabled(True)
+        else:
+            self.line_edit_path.setDisabled(False)
 
     @property
-    def use_default_path(self) -> QCheckBox:
-        return self.default_path_checkBox
+    def download_options(self) -> dict:
+        """returns download options selected by the user"""
+        return self.__state
 
     def __init_state(self):
         self.__state = {
             DOWNLOAD_PATH: {
                 EDIT_LINE_VALUE: '',
                 DEFAULT_PATH_CHECKED: True
-            }
+            },
+            # TODO (video resolution, fps and audio sample rate) use the default one set in the app settings (when
+            #  the settings will be done)
+            VIDEO_RES: SupportedVideoResolution.RES_720p.value,
+            VIDEO_FPS: SupportedVideoFPS.FPS30.value,
+            AUDIO_SAMPLERATE: SupportedAudioSampleRate.SAMP_R_128.value,
+            PROGRESSIVE: False
         }
 
+        self.default_path_checkBox.setChecked(True)
+        for res in SupportedVideoResolution: self.video_res.addItem(res.value)
+        for fps in SupportedVideoFPS: self.video_fps.addItem(fps.value)
+        for sample_rate in SupportedAudioSampleRate: self.audio_sample_rate.addItem(sample_rate.value)
+        self.progressive_checkbox.setChecked(False)
+
     def cancel_option_change(self):
+        """cancels the current changes by reverting the options back to what they were before
+        opening the dialog window"""
         self.line_edit_path.setText(self.__state[DOWNLOAD_PATH][EDIT_LINE_VALUE])
         self.default_path_checkBox.setChecked(self.__state[DOWNLOAD_PATH][DEFAULT_PATH_CHECKED])
+        self.video_res.setCurrentText(self.__state[VIDEO_RES])
+        self.video_fps.setCurrentText(self.__state[VIDEO_FPS])
+        self.audio_sample_rate.setCurrentText(self.__state[AUDIO_SAMPLERATE])
+        self.progressive_checkbox.setChecked(self.__state[PROGRESSIVE])
         self.hide()
 
     def save_changes(self):
         """saves the configuration for futures downloads
         this will reset after the app is closed"""
+
         if (not self.default_path_checkBox.isChecked() and does_path_exist(self.line_edit_path.text())) \
                 or self.default_path_checkBox.isChecked():
             self.__state[DOWNLOAD_PATH][EDIT_LINE_VALUE] = self.line_edit_path.text()
             self.__state[DOWNLOAD_PATH][DEFAULT_PATH_CHECKED] = self.default_path_checkBox.isChecked()
+            self.__state[VIDEO_RES] = self.video_res.currentText()
+            self.__state[VIDEO_FPS] = self.video_fps.currentText()
+            self.__state[AUDIO_SAMPLERATE] = self.audio_sample_rate.currentText()
+            self.__state[PROGRESSIVE] = self.progressive_checkbox.isChecked()
             self.hide()
         else:
             QMessageBox().critical(None, "ERROR",
--- src/utilities/video_utilities.py
+++ src/utilities/video_utilities.py
@@ -11,3 +11,23 @@ class SupportedVideoResolution(enum.Enum):
     RES_1440p = '1440p'
     RES_2160p = '2160p'
     RES_4320p = '4320p'
+
+
+class SupportedVideoFPS(enum.Enum):
+    FPS24 = '24fps'
+    FPS25 = '25fps'
+    FPS30 = '30fps'
+    FPS48 = '48fps'
+    FPS50 = '50fps'
+    FPS60 = '60fps'
+
+
+class SupportedAudioSampleRate(enum.Enum):
+    SAMP_R_44 = '44.1kbps'
+    SAMP_R_48 = '48kbps'
+    SAMP_R_50 = '50kbps'
+    SAMP_R_70 = '70kbps'
+    SAMP_R_128 = '128kbps'
+    SAMP_R_160 = '160kbps'
+    SAMP_R_192 = '192kbps'
+    SAMP_R_320 = '320kbps'
